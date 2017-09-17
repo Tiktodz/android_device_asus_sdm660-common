77fccba0005823687442045fbe65558c488cf112
Ethan Chen <intervigil@gmail.com>
2017-09-16 22:14:36 -0700

    sdm660-common: Add basic USB HAL that reports no status change
    Change-Id: I38bfe869f9b1b66db0e13249f65e438878d06eff
    Signed-off-by: Rizky Benggolo <travarilo@gmail.com>
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- sdm660.mk
+++ sdm660.mk
@@ -436,7 +436,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service.asus_sdm660
 
 # Vibrator
 PRODUCT_PACKAGES += \
