123a3c52917e8a8be8eae1403c9df040f125872f
Michael Bestas <mkbestas@lineageos.org>
2018-05-26 00:49:11 +0300

    sdm660-common: Add powerhint.xml
     * Based on LA.UM.6.2.r1-08700-sdm660.0
     * Required by perf_hint in libqti-perfd-client.so.
    
    Change-Id: Ief38dc242a92de05305452634fe2318e4de42f90

--- sdm660.mk
+++ sdm660.mk
@@ -360,6 +360,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/power-libperfmgr/sdm660_powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 endif
 
+# Powerhint
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
