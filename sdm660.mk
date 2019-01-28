bd77d6118ec760cd53bef7f19ed4b02f745f51f0
kubersharma001 <kubersharma001@gmail.com>
2019-01-29 01:03:28 +0530

    sdm660-common: import excluded-input-devices config
    Change-Id: If17e706c1b3fb46281f032784cd96a29bc14a89a
    Signed-off-by: Tiktodz <ewprjkt@proton.me>

--- sdm660.mk
+++ sdm660.mk
@@ -168,6 +168,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
 
+# Excluded Input devices
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/excluded-input-devices.xml:system/etc/excluded-input-devices.xml
+
 # Freeform Multiwindow
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
