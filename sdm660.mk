40d92e7ea4125839fb395fd38422d9677a30d1d8
Yuanyuan Liu <yuanliu@codeaurora.org>
2019-03-12 11:20:57 -0700

    sdm660-common: Add `wpa_cli`
    Add wpa_cli to PRODUCT_PACKAGES. This binary is needed for WLAN.
    
    CRs-Fixed: 2414661
    Change-Id: I0f34a6e8175d80b1b4050ee4df64c0a7334ad110
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- sdm660.mk
+++ sdm660.mk
@@ -491,7 +491,8 @@ PRODUCT_PACKAGES += \
     libwpa_client \
     WifiOverlay \
     wpa_supplicant \
-    wpa_supplicant.conf
+    wpa_supplicant.conf \
+    wpa_cli
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
