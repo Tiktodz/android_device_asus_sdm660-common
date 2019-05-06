4de7cc1adb3b3150b3bc6b011ddc7edde11ce9eb
codeworkx <daniel.hillenbrand@codeworkx.de>
2019-05-06 18:08:17 +0200

    sdm660-common: build libbthost_if from hardware/qcom
    Change-Id: Ifbaf499a0825c288c642486db455c9350f45fe32

--- sdm660.mk
+++ sdm660.mk
@@ -60,6 +60,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    libbthost_if
+
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
