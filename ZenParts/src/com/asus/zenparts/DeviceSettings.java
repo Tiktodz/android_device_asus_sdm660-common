7fb3384b8cb7e19561f82699348cace33536a316
Sonal Singh <sonal.singh.19993@gmail.com>
2020-06-20 04:52:06 +0000

    X00T: ZenParts: Fix LED brightness preference not showing up
    Change-Id: I246111117655b4ff488da001a1b9ff31a49dca77
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    Signed-off-by: Tiktodz <ewprjkt@proton.me>

--- ZenParts/src/com/asus/zenparts/DeviceSettings.java
+++ ZenParts/src/com/asus/zenparts/DeviceSettings.java
@@ -94,6 +94,7 @@ public class DeviceSettings extends PreferenceFragment implements
 
     private CustomSeekBarPreference mTorchBrightness;
     private VibratorStrengthPreference mVibratorStrength;
+    private NotificationLedSeekBarPreference mLEDBrightness;
     private Preference mKcal;
     private SecureSettingListPreference mSPECTRUM;
     private Preference mAmbientPref;
@@ -119,12 +120,11 @@ public class DeviceSettings extends PreferenceFragment implements
 
         String device = FileUtils.getStringProp("ro.build.product", "unknown");
 
-        if (FileUtils.fileWritable(NOTIF_LED_BLUE_PATH) && FileUtils.fileWritable(NOTIF_LED_RED_PATH) && FileUtils.fileWritable(NOTIF_LED_GREEN_PATH)) {
-            NotificationLedSeekBarPreference notifLedBrightness =
-                    (NotificationLedSeekBarPreference) findPreference(PREF_NOTIF_LED);
-            notifLedBrightness.setOnPreferenceChangeListener(this);
-        } else { getPreferenceScreen().removePreference(findPreference(CATEGORY_NOTIF)); }
-
+        mLEDBrightness = (NotificationLedSeekBarPreference) findPreference(PREF_NOTIF_LED);
+        mLEDBrightness.setEnabled(FileUtils.fileWritable(NOTIF_LED_BLUE_PATH) &&
+              FileUtils.fileWritable(NOTIF_LED_RED_PATH) &&
+                  FileUtils.fileWritable(NOTIF_LED_GREEN_PATH));
+        mLEDBrightness.setOnPreferenceChangeListener(this);
 
         mTorchBrightness = (CustomSeekBarPreference) findPreference(PREF_TORCH_BRIGHTNESS);
         mTorchBrightness.setEnabled(FileUtils.fileWritable(TORCH_1_BRIGHTNESS_PATH) &&
