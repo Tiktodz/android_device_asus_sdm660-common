5ebb92cc03597d0f52cc71e7195fb6fd45fff92e
OdSazib <odsazib@gmail.com>
2020-07-16 00:46:32 +0600

    sdm660-common: Add powerhint for sdm660 and sdm636 * Add EAS_POWERHINT_VARIANT := sdm636 for X00TD in device.mk
    Co-authored-by: Subhajeet Muhuri <kenny3fcb@gmail.com>
    Change-Id: I4694b59561f242d01a0b11a86ebfbfcf9633046f
    Signed-off-by: OdSazib <odsazib@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -363,6 +363,15 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/power-libperfmgr/sdm660_powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 endif
 
+# Powerhint
+ifeq ($(EAS_POWERHINT_VARIANT), sdm636)
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/power-libperfmgr/sdm636_powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+else
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/power-libperfmgr/sdm660_powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+endif
+
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
