6ee8f84415b57b41467396b26fe74783304ddb3f
Vivekachooz <vivekachooz@gmail.com>
2020-08-16 10:47:24 +0530

    sdm660-common: Commonize DT2W node
    Signed-off-by: Vivekachooz <vivekachooz@gmail.com>
    Change-Id: I93d45ca1476b70929f801a328277ec44a3f28bb3

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -132,6 +132,7 @@ TARGET_BOARD_PLATFORM_GPU := qcom-adreno512
 
 # Power
 TARGET_USES_INTERACTION_BOOST := true
+TARGET_TAP_TO_WAKE_NODE := "/proc/tpd_gesture"
 
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
