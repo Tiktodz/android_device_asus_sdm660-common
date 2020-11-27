50fd487bdc354a6d21c8079401d9d75742200b70
Ethan Chen <intervigil@gmail.com>
2020-11-27 20:16:13 +0530

    sdm660-common: Use Soong namespace to avoid device build conflicts
    Change-Id: Id04f464102264b6f2e8d49b39389c7e346613b39

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -162,6 +162,9 @@ include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += $(COMMON_PATH)
+
 # Treble
 BOARD_VNDK_VERSION := current
 
