81c2b2ed26291863ebc6f61d01fa727f941d2209
Rahul Krishna <rahulkrishna585@gmail.com>
2021-01-23 23:34:47 +0530

    sdm660-common: Remove missed QTI bt audio stack blobs
    Change-Id: I4076b185951b5d8f8ba648882f5864e212e03113

--- sdm660.mk
+++ sdm660.mk
@@ -43,7 +43,6 @@ PRODUCT_PACKAGES += \
     libvolumelistener
 
 PRODUCT_PACKAGES += \
-    liba2dpoffload \
     libhdmiedid \
     libhfp \
     libsndmonitor \
@@ -85,11 +84,7 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor
-
-PRODUCT_PACKAGES += \
-    libbthost_if
 
 # Camera
 PRODUCT_PACKAGES += \
