4ba1e6ca7331982e345a151362e51755413b5f5b
arter97 <qkrwngud825@gmail.com>
2021-01-23 18:34:18 +0100

    sdm660-common: init: workaround SafetyNet check * Switch to walleye oreo build fingerprint
    Change-Id: I1f3840a9694dc8e558676d9b4a0a4cea64fd1b2e
    Signed-off-by: aslenofarid <yoniasleno.farid14@gmail.com>
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- init/init_sdm660.cpp
+++ init/init_sdm660.cpp
@@ -36,6 +36,8 @@
 
 #include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <sys/_system_properties.h>
 
 #include "vendor_init.h"
@@ -79,8 +81,58 @@ void NFC_check()
         property_override("ro.hq.support.nfc", "0");
 }
 
+/* From Magisk@jni/magiskhide/hide_utils.c */
+static const char *snet_prop_key[] = {
+  "ro.boot.vbmeta.device_state",
+  "ro.boot.verifiedbootstate",
+  "ro.boot.flash.locked",
+  "ro.boot.veritymode",
+  "ro.boot.warranty_bit",
+  "ro.warranty_bit",
+  "ro.debuggable",
+  "ro.secure",
+  "ro.build.type",
+  "ro.build.tags",
+  NULL
+};
+
+static const char *snet_prop_value[] = {
+  "locked",
+  "green",
+  "1",
+  "enforcing",
+  "0",
+  "0",
+  "0",
+  "1",
+  "user",
+  "release-keys",
+  NULL
+};
+
+static void workaround_snet_properties() {
+    // Hide all sensitive props
+    for (int i = 0; snet_prop_key[i]; ++i) {
+        property_override(snet_prop_key[i], snet_prop_value[i]);
+    }
+}
+
+static void set_build_fingerprint(const char *fingerprint){
+    property_override("ro.bootimage.build.fingerprint", fingerprint);
+    property_override("ro.system.build.fingerprint", fingerprint);
+    property_override("ro.build.fingerprint", fingerprint);
+    property_override("ro.vendor.build.fingerprint", fingerprint);
+}
+
+static void set_build_description(const char *description){
+    property_override("ro.build.description", description);
+}
+
 void vendor_load_properties()
 {
     set_avoid_gfxaccel_config();
     NFC_check();
+    workaround_snet_properties();
+    set_build_fingerprint("google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
+    set_build_description("walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys");
 }
