33c8aeab74d82df30bbc3959c3681158656e9b5c
SonalSingh18 <sonal.singh.19993@gmail.com>
2021-01-26 12:14:49 +0530

    [SQUASH] sdm660-common: Nuke lineage specific stuffs
    sdm660-common: Nuke trust hal
    
    * We aren't lineage...
    
    Signed-off-by: SonalSingh18 <sonal.singh.19993@gmail.com>
    
    sdm660-common: Nuke lineage's livedislay
    
    Signed-off-by: strongreasons <student-jmxdeqtpi@student.sman1sumenep.sch.id>

--- sdm660.mk
+++ sdm660.mk
@@ -238,10 +238,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.asus_sdm660
 
-# LiveDisplay native
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_PACKAGES += \
     android.hardware.media.omx@1.0-impl \
@@ -480,10 +476,6 @@ PRODUCT_BOOT_JARS += \
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
-
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
 
 # USB
 PRODUCT_PACKAGES += \
