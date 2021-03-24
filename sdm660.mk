d458dffda35bdbc6c6f18d6c90d71cd6544431ac
SayanAcharya <rowdysayan99@gmail.com>
2021-03-24 05:45:06 +0000

    sdm660-common: configs: Override perfconfigstore from device tree
    * With deleted memory changes of caf which causes massive performance
      degradation
    
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- sdm660.mk
+++ sdm660.mk
@@ -373,7 +373,8 @@ PRODUCT_PACKAGES += \
 
 # QTI Performance
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
+    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf \
+    $(LOCAL_PATH)/configs/perf/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfconfigstore.xml
 
 # Secure element
 PRODUCT_PACKAGES += \
