48b007041b1659e2e498b5d758fe80894a53b9f0
Jaihind Yadav <jaihindyadav@codeaurora.org>
2021-03-26 18:11:24 +0530

    sdm660-common: Switch to SYSTEM_EXT_<PUBLIC/PRIVATE>_SEPOLICY_DIRS
    BOARD_PLAT_<PUBLIC/PRIVATE>SEPOLICY_DIR is going to be deprecated
    so using new flag.
    
    Change-Id: Ic883ecad5fe08e329f6dcaa5ec3eec9cefa82528
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -157,6 +157,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(COMMON_PATH)
