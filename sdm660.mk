c3cc10b758a56fbf19ed0d7ca785a133d89b5553
Vivekachooz <vivekachooz@gmail.com>
2021-05-22 16:12:43 +0530

    sdm660-common: Added support for both lv and nlv based kernel
    Signed-off-by: fakedotkit <ewprjkt@proton.me>
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- sdm660.mk
+++ sdm660.mk
@@ -445,7 +445,8 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
 
 # VR
 PRODUCT_PACKAGES += \
