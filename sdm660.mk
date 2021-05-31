91d0b7c2e5d43a1586753b4459b59e42d20fc818
ElectroPerf <kunmun.devroms@gmail.com>
2021-05-31 11:07:14 +0000

    X00T: dirac: Add dirac
    * test stage
    
    Change-Id: I92767832f2025e02a95c6e42047342c5045ccd19
    Signed-off-by: ElectroPerf <kunmun.devroms@gmail.com>
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- sdm660.mk
+++ sdm660.mk
@@ -114,6 +114,14 @@ PRODUCT_PACKAGES += \
 # Configstore
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-service
+# Dirac
+PRODUCT_PROPERTY_OVERRIDES += \
+    hpx_send_params=1 \
+    ro.audio.soundfx.dirac=true \
+    persist.audio.dirac.speaker=true \
+    persist.dirac.acs.controller=qem \
+    persist.dirac.acs.storeSettings=1 \
+    persist.dirac.acs.ignore_error=1
 
 # Display
 PRODUCT_PACKAGES += \
