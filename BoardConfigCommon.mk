5bc28bc5889c20c4680d4fa0740a060de0065bc9
TH779 <i@779.moe>
2021-10-07 13:09:53 +0800

    sdm660-common: Allow missing required modules
    Signed-off-by: TH779 <i@779.moe>
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -8,6 +8,7 @@ COMMON_PATH := device/asus/sdm660-common
 
 # Broken Rules
 BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
 # Architecture
