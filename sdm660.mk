ef82c65790b13e76c268aaa369c83bf6784a3731
Vivekachooz <vivekachooz@gmail.com>
2021-10-16 09:27:36 +0530

    sdm660-common: Move surfaceflinger props to vendor.prop
    Change-Id: Ibc0e6c56e994d4ce1d94bcae1c8ad044ca3a1bc5
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- sdm660.mk
+++ sdm660.mk
@@ -411,13 +411,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
-# SurfaceFlinger
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
-    ro.surface_flinger.max_virtual_display_dimension=4096 \
-    ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
-    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
-
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \
