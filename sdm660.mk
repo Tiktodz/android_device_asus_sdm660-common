f37c676ec6c1d612b9568dc646b3c8153c4486e4
Mathieu Chartier <mathieuc@google.com>
2021-11-02 21:37:07 +0530

    sdm660-common: Always preopt extracted APKs
    Change-Id: I2e4742a796b8142f14eaad5eba81eaf800d64914

--- sdm660.mk
+++ sdm660.mk
@@ -409,6 +409,10 @@ PRODUCT_SOONG_NAMESPACES += \
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
+# Always preopt extracted APKs to prevent extracting out of the APK
+# for gms modules.
+PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
+
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \
