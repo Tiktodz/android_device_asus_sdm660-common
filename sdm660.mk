8c2b72db37d0c44281895f1c518d7fd9ef60ec45
HotDogfinba11 <https://olivers-portfolio.co.uk>
2021-12-06 15:51:39 +0530

    sdm660-common: Enable adaptive charging feature
    Change-Id: Ifb26b4033d58e333c278df52c01fa7d8ac246322
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- sdm660.mk
+++ sdm660.mk
@@ -70,6 +70,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
 
+# Adaptive charging
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/adaptivecharging.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/adaptivecharging.xml
+
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService-Soong \
