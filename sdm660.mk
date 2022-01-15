693dcf76f783ca8227a680ff1fd982388a566426
NurKeinNeid <mralexman3000@gmail.com>
2022-01-15 12:10:56 +0100

    sdm660-common: Switch to CAF ANT+ packages
    Change-Id: I40157099aecdb2fdba675affe8b9e6904d7ac31e
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- sdm660.mk
+++ sdm660.mk
@@ -69,11 +69,8 @@ PRODUCT_COPY_FILES += \
 
 # ANT+
 PRODUCT_PACKAGES += \
-    AntHalService \
-    com.dsi.ant.antradio_library
-
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
+    AntHalService-Soong \
+    com.dsi.ant@1.0.vendor
 
 # Bluetooth
 PRODUCT_PACKAGES += \
