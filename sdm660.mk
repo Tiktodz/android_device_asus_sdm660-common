6b51b7c5dab5e7ae0301e579ef9460ebdc453cf8
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2022-02-11 23:20:09 +0530

    sdm660-common: Add powerhint for sdm636
    Change-Id: Ibf5310fd501caf716d75316fe8988a79379dc538
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- sdm660.mk
+++ sdm660.mk
@@ -343,6 +343,10 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
     vendor.qti.hardware.perf@2.2.vendor
 
+# Powerhint
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
