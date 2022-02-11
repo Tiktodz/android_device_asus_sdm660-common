b59cef92d7d3301a8ccb94369adda1c0d2781c44
Steve Kondik <steve@cyngn.com>
2022-02-11 22:49:23 +0530

    sdm660-common: power-libperfmgr: Add support for tap-to-wake feature control
    Signed-off-by: Subhajeet Muhuri <kenny3fcb@gmail.com>
    Change-Id: Ideb02c659d64cdde4ee2f46c2631f7b42d003b05

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -113,7 +113,6 @@ TARGET_BOARD_PLATFORM := sdm660
 
 # Power
 TARGET_USES_INTERACTION_BOOST := true
-TARGET_TAP_TO_WAKE_NODE := "/proc/tpd_gesture"
 
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
