c443717117bca969f63dc0c403650dfe2b5b4995
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2022-02-11 23:20:09 +0530

    sdm660-common: Add powerhint for sdm636
    Change-Id: Ibf5310fd501caf716d75316fe8988a79379dc538

--- sdm660.mk
+++ sdm660.mk
@@ -344,6 +344,10 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.3-service.asus_sdm660-libperfmgr \
     vendor.qti.hardware.perf@2.2.vendor
 
+# Powerhint
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
