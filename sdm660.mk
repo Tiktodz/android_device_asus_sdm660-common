2135039ad6cbb1b5baf665f4e3268deb95f4de2a
sb6596 <shubhamprince111@gmail.com>
2022-02-13 11:04:09 +0530

    sdm660-common: Build mock power.stats HAL
    Signed-off-by: sb6596 <shubhamprince111@gmail.com>
    Change-Id: Ie38a6c8e6b14c9882f8ff162a454be0b3bf8060d

--- sdm660.mk
+++ sdm660.mk
@@ -345,7 +345,8 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.asus_sdm660-libperfmgr
+    android.hardware.power-service.asus_sdm660-libperfmgr \
+    android.hardware.power.stats@1.0-service.mock
 
 # Powerhint
 PRODUCT_COPY_FILES += \
