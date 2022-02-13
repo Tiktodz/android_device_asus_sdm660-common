438497dba5e5f155dbd63883d45fce2d772b0cc6
Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
2022-02-13 10:56:31 +0530

    sdm660-common: Include hardware/google/interfaces and hardware/google/pixel
     * Required by power-libperfmgr
    
    Change-Id: I4053938614be26c7066ea83d97955739f39b4144

--- sdm660.mk
+++ sdm660.mk
@@ -423,7 +423,9 @@ PRODUCT_COPY_FILES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel
 
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
