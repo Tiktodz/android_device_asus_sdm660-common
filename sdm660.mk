00222e532d1c688a708b83a767d7b4afbe8d9e95
Vivekachooz <vivekachooz@gmail.com>
2022-02-13 14:27:14 +0530

    Revert "sdm660-common: Add dummy perf profile."
    This reverts commit f7bcc5396bdbef0a5be14e2cf93b4f8a3b227bd6.
    
    Change-Id: Ib1231392574973a4ab762c262755f6bbdb46673c
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- sdm660.mk
+++ sdm660.mk
@@ -374,10 +374,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libstdc++_vendor
 
-# QTI Performance
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
-
 # Radio
 PRODUCT_PACKAGES += \
     librmnetctl \
