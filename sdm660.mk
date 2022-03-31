3c5948799c2b157d6bcba478548d22244a656f99
kssrao13882 <kssrao13882@gmail.com>
2022-03-31 07:03:28 +0000

    sdm660-common: Import pixel_2016_exclusive.xml
    This allows Unlimited Backup in Google Photos in any Quality
    
    Change-Id: Id449d60b3d1479c428a0fa98a714dd53f44de9ad

--- sdm660.mk
+++ sdm660.mk
@@ -154,6 +154,10 @@ PRODUCT_PACKAGES += \
     qcom.fmradio \
     qcom.fmradio.xml
 
+# Google Photos
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/pixel/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
+
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@2.0-impl-qti \
