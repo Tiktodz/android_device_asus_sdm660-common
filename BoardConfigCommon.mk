f9d1ac22ef8220986aa847284d75ad9b71b7f9ca
Nolen Johnson <johnsonnolen@gmail.com>
2022-05-22 11:35:49 +0530

    sdm660-common: Remove deprecated TARGET_USES_QTI_CAMERA_DEVICE
    * Dead as of https://review.lineageos.org/c/LineageOS/android_vendor_lineage/+/331507.
    * We already build vendor.qti.hardware.camera.device@1.0
      so we have what we need.
    
    Change-Id: I8ec420d6264fee70a1602eecdbb4f34f2e5db3dc
    Signed-off-by: kuplemarkeple <ewprjkt@proton.me>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -44,9 +44,6 @@ TARGET_POWERHAL_MODE_EXT := $(COMMON_PATH)/power-libperfmgr/power/power-mode.cpp
 # Bootloader
 TARGET_NO_BOOTLOADER := true
 
-# Camera
-TARGET_USES_QTI_CAMERA_DEVICE := true
-
 # Display
 TARGET_SCREEN_DENSITY := 420
 BOARD_USES_ADRENO := true
