5e0f171776aca5d0442a656a60d70cf6857275ef
Tiktodz <ewprjkt@proton.me>
2022-09-03 07:45:09 +0000

    sdm660-common: Remove duplicate sf prop
    Change-Id: Ie568d6276ec661b19704839d43ab023bd851bf44

--- sdm660.mk
+++ sdm660.mk
@@ -472,12 +472,6 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
     Settings \
     SystemUI
 
-# SurfaceFlinger
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
-    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
-    ro.surface_flinger.max_virtual_display_dimension=4096
-
 # Sqlite
 PRODUCT_PACKAGES += \
     sqlite3
