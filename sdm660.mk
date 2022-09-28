dd129c7a5bec661ac74c7174d6747924de5c3850
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-29 00:38:17 +0200

    sdm660-common: Build default health HIDL recovery impl
    Change-Id: I9710760a1b7895ed9a0c3da967c4b30ea655bf94
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- sdm660.mk
+++ sdm660.mk
@@ -193,6 +193,7 @@ PRODUCT_PACKAGES += \
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-service
 
 # HIDL
