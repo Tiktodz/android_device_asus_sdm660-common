99c57f2217df2e5752b2680b55cb2dcf82f85329
Tiktodz <ewprjkt@proton.me>
2022-10-23 05:42:14 +0000

    sdm660-common: Bringback doze

--- sdm660.mk
+++ sdm660.mk
@@ -134,6 +134,10 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     android.frameworks.displayservice@1.0.vendor
 
+# Doze mode
+PRODUCT_PACKAGES += \
+    Doze
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
