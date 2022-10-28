c8cb0f808c1bda9e3ea0109c5048298b9cd095c7
Gabriel Biren <gbiren@google.com>
2022-10-28 18:54:04 +0000

    sdm660-common: Use the AIDL WiFi service
    Bug: 205044134
    Test: Presubmit tests
    Change-Id: Icdaa448e2ed59b638056593adb4bcd769f34220d
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- sdm660.mk
+++ sdm660.mk
@@ -477,7 +477,7 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     android.hardware.wifi@1.5.vendor \
     hostapd \
     libwifi-hal-qcom \
