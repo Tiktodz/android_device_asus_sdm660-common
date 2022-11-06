d0bb455598541b7a43dbf6d58ce3dac4859df0b5
Terminator-J <terminator_j@hotbutteredmonkey.com>
2022-11-05 23:26:34 -0700

    sdm660-common: Build & ship ih8sn inline
    Tired of stupid props making stupid safetynet fail
    cuz stupid debug builds but mostly stupid Google...
    
    Change-Id: I6c9eac005bc5a2030df2f01eaea0ed4caf27f67e
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- sdm660.mk
+++ sdm660.mk
@@ -220,6 +220,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
 
+# Ih8sn
+PRODUCT_PACKAGES += \
+    ih8sn
+
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
@@ -454,7 +458,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
-    hardware/google/pixel
+    hardware/google/pixel \
+    vendor/ih8sn
 
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,7 @@
 # Audio
 rm -rf hardware/qcom-caf/msm8998/audio
 git clone https://github.com/karthik/hardware_qcom-caf_msm8998_audio -b 13 hardware/qcom-caf/msm8998/audio
+
+# ih8sn
+rm -rf vendor/ih8sn
+git clone https://github.com/Terminator-J/vendor_ih8sn -b 12.1-test vendor/ih8sn
