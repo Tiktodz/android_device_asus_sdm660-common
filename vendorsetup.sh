be1f7674c7c9cc00686244019d772c723bcc4865
dotkit <ewprjkt@proton.me>
2023-01-26 12:46:45 +0000

    sdm660-common: Regenerate `vendorsetup`
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,31 @@
-# Audio
-rm -rf hardware/qcom-caf/msm8998/audio
-git clone https://github.com/karthik/hardware_qcom-caf_msm8998_audio -b 13 hardware/qcom-caf/msm8998/audio
+echo '----------------------------------'
+echo '| Starting to clone stuffs needed |'
+echo '----------------------------------'
 
-# ih8sn
+echo '--------------------'
+echo '| Cloning Audio HALs |'
+echo '--------------------'
+rm -rf hardware/qcom-caf/msm8998/audio
+git clone https://github.com/wizardprjkt/hardware_qcom-caf_msm8998_audios -b x hardware/qcom-caf/msm8998/audio
+
+echo '--------------------'
+echo '| Cloning Display HALs |'
+echo '--------------------'
+rm -rf hardware/qcom-caf/msm8998/display
+git clone https://github.com/wizardprjkt/hardware_qcom-caf_msm8998_display -b 13x hardware/qcom-caf/msm8998/display
+
+echo '--------------------'
+echo '| Cloning Media HALs |'
+echo '--------------------'
+rm -rf hardware/qcom-caf/msm8998/media
+git clone https://github.com/wizardprjkt/hardware_qcom-caf_msm8998_media -b 13x hardware/qcom-caf/msm8998/media
+
+echo '--------------------'
+echo '| Cloning ih8sn HALs |'
+echo '--------------------'
 rm -rf vendor/ih8sn
 git clone https://github.com/Terminator-J/vendor_ih8sn -b 12.1-test vendor/ih8sn
+
+echo '-------------------------'
+echo '| Cloning all stuffs success |'
+echo '-------------------------'
