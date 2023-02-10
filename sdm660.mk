b5e0a58c61d46dbde768a7c5a6dc4e3febec8cfd
Nolen Johnson <johnsonnolen@gmail.com>
2023-02-10 21:30:05 +0530

    sdm660-common: Cleanup dead targets
    Offending entries:
    android.hardware.audio.effect@2.0-service
    android.hardware.broadcastradio@1.0-service
    android.hardware.media.omx@1.0-impl
    libOmxSwVencHevc
    libcnefeatureconfig
    libgnsspps
    libsynergy_loc_api
    build/make/core/main.mk:1308: error: Build failed.
    
    Change-Id: I0e4d93f4c3be077d49befa1f547d11d623ca74d5
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- sdm660.mk
+++ sdm660.mk
@@ -30,7 +30,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
-    android.hardware.audio.effect@2.0-service \
     android.hardware.audio.service
 
 PRODUCT_PACKAGES += \
@@ -103,10 +102,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.charger.enable_suspend=true
 
-# CNE
-PRODUCT_PACKAGES += \
-    libcnefeatureconfig
-
 # Display
 PRODUCT_PACKAGES += \
     gralloc.sdm660 \
@@ -158,8 +153,7 @@ PRODUCT_PACKAGES += \
 
 # FM
 PRODUCT_PACKAGES += \
-    android.hardware.broadcastradio@1.0-impl \
-    android.hardware.broadcastradio@1.0-service
+    android.hardware.broadcastradio@1.0-impl
 
 PRODUCT_PACKAGES += \
     FM2 \
@@ -188,8 +182,6 @@ PRODUCT_PACKAGES += \
     libbatching \
     libgeofencing \
     libgnss \
-    libgnsspps \
-    libsynergy_loc_api \
     libgps.utils \
     liblocation_api \
     libsensorndkbridge \
@@ -252,7 +244,6 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_PACKAGES += \
-    android.hardware.media.omx@1.0-impl \
     libavservices_minijail \
     libavservices_minijail.vendor
 
@@ -304,7 +295,6 @@ PRODUCT_PACKAGES += \
     libOmxEvrcEnc \
     libOmxG711Enc \
     libOmxQcelp13Enc \
-    libOmxSwVencHevc \
     libOmxVdec \
     libOmxVenc \
     libstagefrighthw \
