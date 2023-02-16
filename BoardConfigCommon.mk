1a439cb1f52e19065028078615454d635e5bbbb7
Nolen Johnson <johnsonnolen@gmail.com>
2023-02-16 11:16:37 +0530

    sdm660-common: Drop vestigial FM board-flags
    * No longer read anywhere.
    
    Change-Id: Iaf5d8cda03e055c4331e3a20e33c8058037b09b2
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -65,10 +65,6 @@ TARGET_EXFAT_DRIVER := exfat
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 
-# FM
-BOARD_HAS_QCA_FM_SOC := cherokee
-BOARD_HAVE_QCOM_FM := true
-
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE += earlycon=msm_serial_dm,0xc170000 androidboot.hardware=qcom
