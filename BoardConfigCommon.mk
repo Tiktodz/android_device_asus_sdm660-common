87bc97bcd7e20a15062e2c465031a56adfda0fc5
Charsi-Tejas <tejas@m.ms.evolution-x.org>
2023-05-29 00:17:48 +0800

    sdm660-common: Compile HWUI
    * Activates few optimizing compiler flags for HWUI.
    * -fno-omit-frame-pointer, -marm, -mapcs
    
    Change-Id: Ia1391b1a6cbfd876459042f88e58f2eafa17d9d6
    Signed-off-by: Pascoato <viniciuspascoato@gmail.com>
    Signed-off-by: Charsi-Tejas <tejas@m.ms.evolution-x.org>
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -53,6 +53,9 @@ TARGET_ENABLE_MEDIADRM_64 := true
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 
+# HWUI
+HWUI_COMPILE_FOR_PERF := true
+
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE += earlycon=msm_serial_dm,0xc170000 androidboot.hardware=qcom
