a098905b51ce5127249d05cbf5a4c8b0c8c607be
dotkit <ewprjkt@proton.me>
2023-08-09 07:13:36 +0000

    sdm660-common: updated some changes to bind with qpr3
    Change-Id: I465bb7c5a7e1df11049a1052996527d1d77ab12a

--- sdm660.mk
+++ sdm660.mk
@@ -448,19 +448,6 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel
 
-# Speed profile services and wifi-service to reduce RAM and storage
-PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
-
-# Always preopt extracted APKs to prevent extracting out of the APK
-# for gms modules.
-PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
-PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
-PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
-PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-USE_DEX2OAT_DEBUG := false
-WITH_DEXPREOPT_DEBUG_INFO := false
-
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     Settings \
     SystemUI
