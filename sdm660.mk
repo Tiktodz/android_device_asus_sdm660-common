0a534f6f15537392eff8ce602bb8d94432f7ad32
dotkit <ewprjkt@proton.me>
2023-08-22 12:16:44 +0000

    sdm660-common: overlay: lineage ~> qassa
    Change-Id: I8990d1e03bc09ec40167231e27145531fca1fcf9
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- sdm660.mk
+++ sdm660.mk
@@ -298,7 +298,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage \
+    $(LOCAL_PATH)/overlay-qassa \
     $(LOCAL_PATH)/overlay-system
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
