e4dfff25ee8e258295b4d1fd195138eddc54547c
dotkit <ewprjkt@proton.me>
2023-08-24 16:59:37 +0000

    Revert "sdm660-common: Add touchscreen gestures HIDL HAL"
    This reverts commit 3089e03074bd6238a4df962b8a3548c61969b072.

--- sdm660.mk
+++ sdm660.mk
@@ -477,10 +477,6 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
 
-# Touch
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.asus_sdm660
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.asus_sdm660
