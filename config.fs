3e123b6a33c291d4f192dc4a2f55efcde6a46c68
Vivekachooz <vivekachooz@gmail.com>
2023-09-16 15:49:56 +0530

    sdm660-common: Add symlinks for firmware and dsp
    Change-Id: Idb2dea0b34aa308fa4cba7d217d4f34122577cde
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- config.fs
+++ config.fs
@@ -25,6 +25,30 @@ value:2908
 [AID_VENDOR_QTR]
 value:2909
 
+[dsp/]
+mode: 0771
+user: AID_MEDIA
+group: AID_MEDIA
+caps: 0
+
+[firmware/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[persist/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[vendor/bin/cnd]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE NET_ADMIN BLOCK_SUSPEND
+
 [vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti]
 mode: 0755
 user: AID_BLUETOOTH
