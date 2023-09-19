c90afc9f8749d172abe8fab4fb45e04158bc6947
dotkit <ewprjkt@proton.me>
2023-09-19 12:54:26 +0000

    sdm660-common: Update vendorsetup
    Change-Id: Ia8b2f16df6d6eeb6cc38c1063d18eacde3fedca9

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,5 @@
 rm -rf kernel/asus/sdm660
-git clone --recursive https://github.com/Tiktodz/android_kernel_asus_sdm636 -b 10 kernel/asus/sdm660
+git clone --recursive https://github.com/Tiktodz/android_kernel_asus_sdm636 -b codelinaro-hmp kernel/asus/sdm660
 
 rm -rf hardware/qcom-caf/msm8998/audio
 git clone https://github.com/Tiktodz/hardware_qcom-caf_msm8998_audio -b ten hardware/qcom-caf/msm8998/audio
-
-rm -rf vendor/qcom/opensource/power
-git clone https://github.com/Tiktodz/vendor_qcom_opensource_power -b 10 vendor/qcom/opensource/power
