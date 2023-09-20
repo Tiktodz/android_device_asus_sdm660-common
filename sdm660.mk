67a44141ddeb604e1b4ad696d9f4625a16c1b28e
dotkit <ewprjkt@proton.me>
2023-09-20 17:13:45 +0000

    sdm660-common: Move surfaceflinger to vendor props
    Change-Id: I520c8355f606fbfbf568a6c3623d8683660b4ab1

--- sdm660.mk
+++ sdm660.mk
@@ -434,13 +434,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
-# SurfaceFlinger
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
-    ro.surface_flinger.max_virtual_display_dimension=4096 \
-    ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
-    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
-
 # Sqlite
 PRODUCT_PACKAGES += \
     sqlite3
