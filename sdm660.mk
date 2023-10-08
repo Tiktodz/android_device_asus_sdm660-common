640fcb5edac6ed97ebf5b8f921cb478f1ba2e149
Roberto Sartori <roberto.sartori.android@gmail.com>
2023-10-08 19:07:37 +0530

    sdm660-common: Build android.frameworks.sensorservice
     * Needed to get cameras working on Android 14.
    
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- sdm660.mk
+++ sdm660.mk
@@ -76,6 +76,8 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0 \
+    android.frameworks.sensorservice@1.0.vendor \
     android.hardware.camera.common@1.0 \
     android.hardware.camera.device@3.4 \
     android.hardware.camera.provider@2.4-impl \
