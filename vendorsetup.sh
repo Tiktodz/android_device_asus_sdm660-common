eeeae07e0cdfd155679dfab77a3cbfad0ee0de0e
fakedotkit <ewprjkt@proton.me>
2023-10-16 05:03:12 +0000

    sdm660-common: Update as per source side
    Signed-off-by: fakedotkit <ewprjkt@proton.me>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
-rm -rf kernel/asus/sdm660
+#rm -rf kernel/asus/sdm660
 git clone --recursive https://github.com/Tiktodz/android_kernel_asus_sdm636 -b codelinaro-hmp kernel/asus/sdm660
 
 rm -rf hardware/qcom-caf/msm8998/audio
-git clone https://github.com/Tiktodz/hardware_qcom-caf_msm8998_audio -b ten hardware/qcom-caf/msm8998/audio
+git clone https://github.com/aslenolineagestuff/android_hardware_qcom_audio -b lineage-17.1-caf-msm8998 hardware/qcom-caf/msm8998/audio
