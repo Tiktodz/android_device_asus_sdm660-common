0cbb20322299b33f15a0a1f5fd56fe7be0df68db
Tiktodz <ewprjkt@proton.me>
2023-11-01 12:53:16 +0000

    Revert "sdm660-common: Update dexpreopt configuration"
    This reverts commit c095f561e7eb29f92364688dd33a21b9a61f4c99.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -82,9 +82,10 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 # Enable dexpreopt to speed boot time
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
-    WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := false
+    WITH_DEXPREOPT := true
   endif
 endif
+WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
 
 # FM
 BOARD_HAS_QCA_FM_SOC := cherokee
