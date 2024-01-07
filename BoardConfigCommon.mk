1da58cc513a757490aaa057780728bf1f0493673
dotkit <ewprjkt@proton.me>
2024-01-07 12:33:18 +0000

    sdm660-common: Switch to double_tap_enable node path
    Change-Id: I2dae5290aba27c4905581c4e8ca2a70232732fc2
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -125,7 +125,7 @@ TARGET_ENFORCES_QSSI := true
 
 # Power
 TARGET_USES_INTERACTION_BOOST := true
-TARGET_TAP_TO_WAKE_NODE := "/proc/tpd_gesture"
+TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
