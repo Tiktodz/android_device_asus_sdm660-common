097ea27587dc91ed658e8d9d1f97e8efebadbd80
Tiktodz <ewprjkt@proton.me>
2024-02-25 12:43:52 +0000

    sdm660-common: Disable hmp sched
    Change-Id: Ib817ccef397aeae22511e5a950b4cb16bd15f117
    Signed-off-by: Tiktodz <ewprjkt@proton.me>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -65,7 +65,7 @@ HWUI_COMPILE_FOR_PERF := true
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE += earlycon=msm_serial_dm,0xc170000 androidboot.hardware=qcom
 BOARD_KERNEL_CMDLINE += user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
-BOARD_KERNEL_CMDLINE += sched_enable_hmp=1 sched_enable_power_aware=1
+BOARD_KERNEL_CMDLINE += sched_enable_power_aware=1
 BOARD_KERNEL_CMDLINE += service_locator.enable=1 loop.max_part=7
 BOARD_KERNEL_CMDLINE += printk.devkmsg=on
 #BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
