0f844dce994bffdde9f1162e69faecea80264483
Tiktodz <ewprjkt@proton.me>
2024-02-27 21:46:39 +0000

    Revert "sdm660-common: Back to proc/tpd_gesture"
    This reverts commit e4b3072994d82ff2f120bfdc99de7685fb2a9f61.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -125,7 +125,6 @@ TARGET_ENFORCES_QSSI := true
 
 # Power
 TARGET_USES_INTERACTION_BOOST := true
-TARGET_TAP_TO_WAKE_NODE := "/proc/tpd_gesture"
 
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
