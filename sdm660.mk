368fe912751190224703b1156df0b3cb3842922e
Kneba <abenkenary3@gmail.com>
2024-03-03 13:03:19 +0000

    sdm660-common: Build Extra VNDK Versions
    Change-Id: Icc9e89039ee90ad91251fa64104ebad8f8c0a54b

--- sdm660.mk
+++ sdm660.mk
@@ -460,6 +460,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v33.so
 
+# VNDK Extra
+PRODUCT_EXTRA_VNDK_VERSIONS := 30 31 32
+
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
