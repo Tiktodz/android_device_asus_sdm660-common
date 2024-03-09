0b64aea2f2d43f324a8bf6323038ec159eca5218
Meisie Widuri <ewprjkt@proton.me>
2024-03-09 16:59:16 +0700

    sdm660-common: Don't nuke aperture

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -6,7 +6,6 @@ LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
     AmbientSensePrebuilt \
-    Aperture \
     AmbientStreaming \
     arcore \
     Camera2 \
