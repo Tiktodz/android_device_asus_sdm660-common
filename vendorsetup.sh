cbf7c8c72e33c2b766971e3d9d7c2343b1cc0dc4
Meisie Widuri <ewprjkt@proton.me>
2024-03-27 04:09:29 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,7 @@
+rm -rf device/asus/X00T
+rm -rf vendor/asus
+rm -rf system/core
+
 # Audio
 rm -rf hardware/qcom-caf/msm8998/audio
 git clone https://github.com/lineageX00T/hardware_qcom-caf_msm8998_audio hardware/qcom-caf/msm8998/audio
@@ -6,7 +10,7 @@ git clone https://github.com/lineageX00T/hardware_qcom-caf_msm8998_audio hardwar
 git clone https://github.com/Tiktodz/device_asus_X00TD -b nad device/asus/X00TD
 
 # Kernel
-git clone --recursive https://github.com/Tiktodz/android_kernel_asus_sdm636 -b codelinaro-hmp kernel/asus/sdm660
+git clone https://github.com/aslenofarid/kernel_asus_sdm660 -b lineage-17.1 kernel/asus/sdm660
 
 # LiveDisplay
 rm -rf hardware/lineage/livedisplay
@@ -18,3 +22,5 @@ git clone https://github.com/LineageOS/android_external_skia -b lineage-17.1 ext
 
 # Vendor
 git clone https://github.com/Tiktodz/android_vendor_asus -b lineage-17.1 vendor/asus
+
+git clone https://github.com/lineageX00T/android_system_core system/core
