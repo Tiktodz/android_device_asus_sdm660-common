b9d42a0b8a5d195f4f764c98f8df432eac3665e2
Meisie Widuri <ewprjkt@proton.me>
2024-03-27 05:02:07 +0700

    Update init_sdm660.cpp

--- init/init_sdm660.cpp
+++ init/init_sdm660.cpp
@@ -139,6 +139,6 @@ void vendor_load_properties()
     set_avoid_gfxaccel_config();
     NFC_check();
     workaround_snet_properties();
-    set_build_fingerprint("google/redfin/redfin:14/UP1A.231105.001/10817346:user/release-keys");
-    set_build_description("redfin-user 14 UP1A.231105.001 10817346 release-keys");
+    set_build_fingerprint("google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
+    set_build_description("walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys");
 }
